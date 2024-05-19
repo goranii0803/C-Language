8cdbff2ed65f469b06e7b45d39d7bd78fcd69d6e
goranii0803 <godgoranii@gmail.com>
2024-05-19 17:58:37 +0900

    commit1883

--- test.c
+++ test.c
@@ -1,19 +1,22 @@
-// #1882
-#include<stdio.h>
-int main(){
-    int n,i;
-    scanf("%d", &n);
-    for(i=1;i<=n;i++){
-        
-        for(int j = 0;j<i;j++) {
+// #1883
+#include <stdio.h>
+int main () {
+    int a,i;
+    scanf("%d", &a);
+    for(i=1;i<=a;i++){
+        for(int j=0;j<a-i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++){
             printf("*");
         }
         printf("\n");
-    
     }
-    for(i=n-1;i>=1;i--){
-        
-        for(int j = 0;j<i;j++){
+    for(i=1;i<a;i++){
+        for(int j=0;j<i;j++){
+            printf(" ");
+        }
+        for(int k=0;k<a-i;k++){
             printf("*");
         }
         printf("\n");
