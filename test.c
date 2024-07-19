d8c69e8f7a66c7ee4a1e4784b1311584b54b066e
goranii0803 <godgoranii@gmail.com>
2024-07-19 20:11:15 +0900

    2차원 배열 총합 구하기

--- test.c
+++ test.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 int main() {
-    int jumsu[2][3]={0},i,j;
-    printf("input\n");
+    int jumsu[2][3]={10,20,30,40,50,60},i,j,sum =0;
     for(i=0;i<2;i++) {
-        for (j=0;j<3;j++) {
-            scanf("%d", &jumsu[i][j]);
-        } 
-    }
-    printf("2차원 배열 출력\n");
-    for(i=0;i<2;i++){
-        for(j=0;j<3;j++){
-            printf("%d",jumsu[i][j]);
+        for(j=0;j<3;j++) {
+            printf("%3d",jumsu[i][j]);
+            sum+=jumsu[i][j];
         }
         printf("\n");
     }
+    printf("sum is %3d \n", sum);
 }
